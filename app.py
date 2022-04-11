1da6f834fe63fb091249c3fc5fca0acc5b445e63
Alper Reha YAZGAN <alperreha@gmail.com>
2022-04-11 11:23:58 +0300

    gui added

--- app.py
+++ app.py
@@ -1,69 +1,74 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import cmath
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import cm
 from matplotlib.patches import ConnectionPatch
-import os, sys
+import sys
+from PyQt6.QtWidgets import (
+    QApplication
+    ,QMessageBox
+    ,QWidget
+    ,QLabel
+    ,QPushButton
+    ,QVBoxLayout
+    ,QTableWidget
+    ,QTableWidgetItem
+    ,QLineEdit
+)
+
+
+#%% Class for Circle that holds complex number for fourier transform 
+class Circle():
+    z : complex
+    
+    def __init__(self, i, complex_number_str):
+        # complex number
+        self.z = complex(complex_number_str)
+        # complex number in polar coordinates
+        (self.r, self.angle) = cmath.polar(self.z)
+
+        # r is too big to be displayed in table so r * 0.1
+        self.r = 4/( (2*i + 1)* np.pi)
+
 
 #%% Class for Fourier Series
-
 class FS():
-
-    Circles = [0,0,0,0,0]
     
-    def __init__(self, Circles, Cycles):
+    def __init__(self, Circles, Cycles, Freq):
         
         self.Circles = Circles
-        self.Cycles  = Cycles
+        self.Cycles : int = Cycles
+        self.Freq   : float = Freq
         
-    #%% X coordinate of circle's center
-    
-    def Xcenter(self, n, theta):
-        
-        ''' 
-            X coordinate of n th circle
-        '''
-        
+    #%% X coordinate of n circle's center
+    def Xcenter(self, n,circles, theta):
         Ans = 0
-        
-        if n > 0:
-            for i in range(1, n + 1):
-                Ans += (4/( (2*i - 1)* np.pi))* np.cos( (2*i - 1)* theta)
-    
+
+        # for inside self.Circles first n circles
+        for i in range(0, n):
+            Ans += circles[i].r * np.cos((2*i-1) *theta)
+
         return Ans
     
     #%% Y coordinate of circle's center
-    
-    def Ycenter(self, n, theta):
-        
-        ''' 
-            Y coordinate of n th circle
-        '''
-        
+    def Ycenter(self, n,circles, theta):
         Ans = 0
-        
-        if n > 0:
-            for i in range(1, n + 1):
-                Ans += (4/( (2*i - 1)* np.pi))* np.sin( (2*i - 1)* theta)
-    
+
+        # for inside self.Circles first n circles
+        for i in range(0, n):
+            Ans += circles[i].r * np.sin((2*i-1) * theta)
         return Ans
     
     #%% Radius of circle
-    
-    def Rds(self, n):
-        
-        ''' 
-            Radius of n th circle
-        '''
-        
-        return 4/( (2*n + 1)* np.pi)
+    def Rds(self, n,circles):
+        return circles[n].r
     
     #%% Plot
     
     def PlotFS(self):
-
         time = np.linspace(0, self.Cycles, self.Cycles* 140)
         
         fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(80, 60))
@@ -74,7 +79,7 @@ class FS():
         for t in time:
             
             thta = 2* np.pi* t
-            
+
             #%% clear the plot
             axs[0].clear()
             
@@ -83,21 +88,21 @@ class FS():
             
             #%% First plot
             
-            for i, circle_radius, c in zip(range(0,len(self.Circles)),self.Circles, color):
-                xc = self.Xcenter(circle_radius, thta)
-                yc = self.Ycenter(circle_radius, thta)
-                R  = self.Rds(circle_radius)
-
+            for i, c in zip(range(0, len(self.Circles)), color):
+                xc = self.Xcenter(i, self.Circles,thta)
+                yc = self.Ycenter(i, self.Circles, thta)
+                R  = self.Rds(i, self.Circles)
+                
                 crl = plt.Circle((xc, yc), R, color=c, alpha = 0.5, linewidth = 2)
                 axs[0].add_artist(crl)
-
+                
                 if (i > 0):
                     axs[0].plot([xco, xc], [yco, yc], color='b', linewidth = 2)
                 
                 xco = xc
                 yco = yc
                 Ro  = R
-
+            
             axs[0].axis('square')
             axs[0].set_xlim([ -9/np.pi, 9/np.pi ])
             axs[0].set_ylim([ -9/np.pi, 9/np.pi ])
@@ -116,35 +121,70 @@ class FS():
         
             #%% Line
             
-            con = ConnectionPatch( xyA = (t,  yc), xyB = (xco, yco), 
+            con = ConnectionPatch( xyA = (t,  yc), xyB = (xc, yc), 
                                    coordsA = 'data', coordsB = 'data',
                                    axesA = axs[1], axesB = axs[0], 
                                    color = 'red')
             axs[1].add_artist(con)
             
             plt.pause(1e-11)
+            pass
 
-#%% Main
 
-if __name__ == '__main__':
-    # get circle_radius1, circle_radius2, circle_radius3, circle_radius4, circle_radius5 from os.argv
-    # if argv length is not 5, print error message and exit
-    if len(sys.argv) != 6:
-        print('Usage: python app.py circle_radius1 circle_radius2 circle_radius3 circle_radius4 circle_radius5')
-        sys.exit()
-        pass
+app         = QApplication([])
+# detect system os and set Style
+app.setStyle('macos')
 
-    circle_radius1 = int(os.sys.argv[1])
-    circle_radius2 = int(os.sys.argv[2])
-    circle_radius3 = int(os.sys.argv[3])
-    circle_radius4 = int(os.sys.argv[4])
-    circle_radius5 = int(os.sys.argv[5])
+window      = QWidget()
+v_layout    = QVBoxLayout()
+# set 720 width and 360 height
+window.setFixedSize(720, 360)
 
-    circles = [circle_radius1, circle_radius2, circle_radius3, circle_radius4, circle_radius5]
 
-    # sort before using
-    circles.sort()
+# create button and 1x5 table only takes integer params like 1 1 1 1 1
+table = QTableWidget(1, 5)
+btn2 = QPushButton('Visualise')
 
-    # Circles, Cycles
-    fs = FS(circles, 2)
+# set the table header
+table.setHorizontalHeaderLabels(['Coefficient 1', 'Coefficient 2', 'Coefficient 3', 'Coefficient 4', 'Coefficient 5'])
+
+
+v_layout.addWidget(QLabel('18011020 - DSP Fourier Transform Visualiser'))
+v_layout.addWidget(QLabel('Fill the coefficients like: 2 3+j 2-2j 2 1'))
+# table column default values
+table.setItem(0, 0, QTableWidgetItem('2'))
+table.setItem(0, 1, QTableWidgetItem('3+j'))
+table.setItem(0, 2, QTableWidgetItem('2-2j'))
+table.setItem(0, 3, QTableWidgetItem('2'))
+table.setItem(0, 4, QTableWidgetItem('1'))
+
+# add float input field to take frequency value
+freq = QLineEdit()
+# set label
+freq.setPlaceholderText('Frequency: 1.0 Hz')
+
+v_layout.addWidget(freq)
+v_layout.addWidget(table)
+v_layout.addWidget(btn2)
+
+def on_btn2_clicked():
+    # Circles empty list
+    app_circles = []
+
+    # read from table values to create circle
+    for i in range(0, 5):
+        app_circles.append(Circle(i,table.item(0, i).text()))
+    
+    # get frequency value
+    freq_val = float(freq.text())
+
+    fs = FS(app_circles, 3, freq_val)
     fs.PlotFS()
+
+btn2.clicked.connect(on_btn2_clicked)
+
+window.setLayout(v_layout)
+window.show()
+app.exec()
+
+
